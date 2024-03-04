1518b96adb9da9c025144f7ad0b43c287548e2d9
Thiago Martins <thiagolealmartins@gmail.com>
2024-03-04 11:29:02 -0300

    adicionando handlers da aula

--- src/main/java/com/thiago/mensageria/exceptionHandler/Handler.java
+++ src/main/java/com/thiago/mensageria/exceptionHandler/Handler.java
@@ -1,10 +1,14 @@
 package com.thiago.mensageria.exceptionHandler;
 
+import org.springframework.cglib.proxy.UndeclaredThrowableException;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
@@ -14,7 +18,7 @@ import jakarta.annotation.Resource;
 public class Handler extends ResponseEntityExceptionHandler{
 
 	@Resource
-	private MessageSource mensaSource;
+	private MessageSource messageSource;
 	private HttpHeaders headers() {
 		HttpHeaders head = new HttpHeaders();
 		head.setContentType(MediaType.APPLICATION_JSON);
@@ -28,4 +32,21 @@ public class Handler extends ResponseEntityExceptionHandler{
 		error.setStatusCode(status.value());
 		return error;
 	}
+	
+	@ExceptionHandler(Exception.class)
+    private ResponseEntity<Object> handleGeneral(Exception e, WebRequest request) {
+        if (e.getClass().isAssignableFrom(UndeclaredThrowableException.class)) {
+            UndeclaredThrowableException exception = (UndeclaredThrowableException) e;
+            return handleBusinessException((MensagemNotSendException) exception.getUndeclaredThrowable(), request);
+        } else {
+            String message = messageSource.getMessage("error.server", new Object[]{e.getMessage()}, null);
+            ResponseError error = response(message,HttpStatus.INTERNAL_SERVER_ERROR);
+            return handleExceptionInternal(e, error, headers(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+        }
+    }
+    @ExceptionHandler({MensagemNotSendException.class})
+    private ResponseEntity<Object> handleBusinessException(MensagemNotSendException e, WebRequest request) {
+        ResponseError error = response(e.getMessage(),HttpStatus.CONFLICT);
+        return handleExceptionInternal(e, error, headers(), HttpStatus.CONFLICT, request);
+    }
 }
